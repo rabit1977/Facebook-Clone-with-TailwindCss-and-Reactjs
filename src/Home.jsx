307ed291858c717fe8406a3c51cd7b6dc7078599
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-12-27 23:23:35 +0100

    ... made some minor updates ...

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import SidebarRight from './components/SidebarRight';
 
 export default function Home() {
   return (
-    <div className=' w-full lg:gap-20 gap-4 flex py-2 px-4'>
+    <div className='flex py-2 px-4'>
       <Sidebar />
       <Main />
       <SidebarRight />
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Main() {
   return (
-    <div className='relative flex-2 bg-white h-72 rounded-lg shadow-sm  lg:max-w-[540px] max-w-[450px] m-auto my-4'>
+    <div className='relative flex-1 bg-white h-72 rounded-lg shadow-sm md:max-w-xl lg:max-w-2xl sm:grow max-w-xl m-auto my-4'>
       <div className='absolute bg-white rounded-full p-3 z-50 left-8 top-40 cursor-pointer hover:bg-[#F0F2F5]'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -57,7 +57,17 @@ export default function Main() {
         </div>{' '}
       </div>
       <div className='flex h-full max-h-52 px-3'>
-        <div className='flex justify-between gap-2.5 pt-4'>
+        <div className='flex justify-between gap-2.5 pt-4 overflow-hidden'>
+          <div className='flex flex-col rounded-xl overflow-hidden items-center justify-end bg-black'>
+            <img
+              className='h-full object-cover object-left opacity-70 hover:scale-[104%] hover:origin-center hover:linear duration-300 cursor-pointer'
+              src='public/familyimage.jpg '
+              alt=''
+            />
+            <p className='absolute text-white/80 pb-3 font-[600px] text-lg tracking-wide'>
+              2.3k
+            </p>
+          </div>
           <div className='flex flex-col rounded-xl overflow-hidden items-center justify-end bg-black'>
             <img
               className='h-full object-cover object-left opacity-70 hover:scale-[104%] hover:origin-center hover:linear duration-300 cursor-pointer'
